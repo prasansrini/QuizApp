a540feadb77ea46debc09ee4e4dde10452914597
Prasanna <github@prasansrini.com>
2024-04-15 21:13:18 +0200

    Fixed unit tests.

--- src/test/java/com/quiz/myapp/MyQuizApp/util/QuizQuestionTranslatorUtilTest.java
+++ src/test/java/com/quiz/myapp/MyQuizApp/util/QuizQuestionTranslatorUtilTest.java
@@ -14,12 +14,8 @@ class QuizQuestionTranslatorUtilTest {
     @Test
     void getTranslated() {
 
-        QuizQuestion question = new QuizQuestion(
-                "Question 1",
-                new String[]{"option1", "option2", "option3", "option4"},
-                "answer",
-                true
-        );
+        QuizQuestion question = new QuizQuestion("Question 1", new String[]{"option1", "option2", "option3", "option4"},
+                "answer", true);
 
         assertQuizQuestion(question);
     }
@@ -43,14 +39,8 @@ class QuizQuestionTranslatorUtilTest {
     private List<QuizQuestion> loadQuizQuestions() {
         List<QuizQuestion> questions = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            QuizQuestion question = new QuizQuestion(
-                    "Question " + i,
-                    new String[]{
-                            "option 1", "option 2", "option 3", "option 4"
-                    },
-                    "answer " + i,
-                    true
-            );
+            QuizQuestion question = new QuizQuestion("Question " + i,
+                    new String[]{"option 1", "option 2", "option 3", "option 4"}, "answer " + i, true);
             questions.add(question);
         }
 
