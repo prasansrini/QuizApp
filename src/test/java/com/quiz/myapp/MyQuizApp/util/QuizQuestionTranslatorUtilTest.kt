88be8615a924aa5ade4242c2df74beb84176a7d9
Prasanna <github@prasansrini.com>
2024-04-17 16:33:19 +0200

    Added Kotlin support and updated unit tests.

--- src/test/java/com/quiz/myapp/MyQuizApp/util/QuizQuestionTranslatorUtilTest.kt
+++ src/test/java/com/quiz/myapp/MyQuizApp/util/QuizQuestionTranslatorUtilTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.util.function.Consumer
 
-internal class QuizQuestionTranslatorUtilTest {
+class QuizQuestionTranslatorUtilTest {
 
     @Test
     fun translatedTest() {
