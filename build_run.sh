2ea970135b63ae82fc59517f72346a398d6592b5
Prasanna Srinivasan <prasanna.srinivasan@mittlogik.se>
2024-07-31 10:37:45 +0200

    Fix script for automated build.

--- build_run.sh
+++ build_run.sh
@@ -2,9 +2,13 @@
 
 mvn clean install -DskipTests || exit
 docker build -t quiz-app . || exit
-docker tag quiz-app prasansrini29/quiz-app || exit
-docker login -u prasansrini29 || exit
-docker push prasansrini29/quiz-app || exit
+
+if [ $# -eq 0 ]
+    then
+        docker tag quiz-app prasansrini29/quiz-app || exit
+        docker login -u prasansrini29 || exit
+        docker push prasansrini29/quiz-app || exit
+fi
 
 # Enable this if Docker compose is not used.
 #docker run --name quiz-app-docker -p 8888:8888 quiz-app || exit
