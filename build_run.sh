5552c8c75684db5278fef50173f36bb721a341a2
Prasanna Srinivasan <prasanna.srinivasan@mittlogik.se>
2024-07-31 11:46:30 +0200

    Updated script with password check.

--- build_run.sh
+++ build_run.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-mvn clean install -f ./build/QuizApp/pom.xml -DskipTests || exit
-docker build -t quiz-app ./build/QuizApp || exit
-
-if [ $# -eq 0 ]
+if [ "$1" == "" ]
     then
-        docker tag quiz-app prasansrini29/quiz-app || exit
-        docker login -u prasansrini29 || exit
-        docker push prasansrini29/quiz-app || exit
+    echo "Usage: ./build_run.sh <DOCKER_HUB_PASSWORD>"
+    exit
 fi
+
+mvn clean install -DskipTests || exit
+docker build -t quiz-app . || exit
+
+docker tag quiz-app prasansrini29/quiz-app || exit
+docker login -u prasansrini29 -p $1 || exit
+docker push prasansrini29/quiz-app || exit
 
 # Enable this if Docker compose is not used.
 #docker run --name quiz-app-docker -p 8888:8888 quiz-app || exit
