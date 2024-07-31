35adab95430c47e632333fe9f59307100ad2261d
Prasanna Srinivasan <prasanna.srinivasan@mittlogik.se>
2024-07-31 10:08:11 +0200

    Added support for pusing Docker image to Docker hub.

--- build_run.sh
+++ build_run.sh
@@ -2,6 +2,9 @@
 
 mvn clean install -DskipTests || exit
 docker build -t quiz-app . || exit
+docker tag quiz-app prasansrini29/quiz-app
+docker login -u prasansrini29
+docker push prasansrini29/quiz-app
 
 # Enable this if Docker compose is not used.
 #docker run --name quiz-app-docker -p 8888:8888 quiz-app || exit
